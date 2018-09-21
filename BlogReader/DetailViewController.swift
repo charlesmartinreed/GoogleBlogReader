f5921a1f1e97ab1446392c1b7c599ea894163cde
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-20 23:20:51 -0500

    Initial Commit

---
+++ BlogReader/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  BlogReader
+//
+//  Created by Charles Martin Reed on 9/20/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
